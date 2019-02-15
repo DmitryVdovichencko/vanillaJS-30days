506d4c9f5c19fc39b97f65e9ad8d762407b024a6
Dmitry Vdovichencko <Kenar89@gmail.com>
2019-02-15 06:45:40 +0500

    Updated with string template

--- 01-JS-music-kit/play.js
+++ 01-JS-music-kit/play.js
@@ -2,33 +2,29 @@
 
 const findAudioEl=(event)=>{
 	let audioSelector;
-	console.log(event.type);
-	if (event.type==="click"){
-		audioSelector = 'audio[data-key=' + '"' + event.target.getAttribute('data-key') + '"]';
-		console.log(audioSelector);
-	}
-	else if (event.type==="keydown"){
-		audioSelector = 'audio[data-key=' + '"' + event.keyCode + '"]';
-	}
-	else{
-		help('.help',true);
-	};
+
+
+	audioSelector = `audio[data-key="${event.keyCode}"]`;
+
 	
+
 	try{
     	
     	playAudio(audioSelector);
     }
     catch(e){
+    	console.log(e);
     	help('.help',true);
 
     }
 };
 const findKeyEl=(event)=>{
-	let keySelector= '.key[data-key=' + '"' + event.keyCode + '"]';
+	let keySelector= `.key[data-key="${event.keyCode}"]`;
 	try{
   		animateKey(keySelector);
     }
     catch(e){
+    	console.log(e);
     	help('.help',true);
   
     }
@@ -66,5 +62,5 @@ const playSound=(event)=>{
 };
 // document.onkeydown = playSound;
 
-document.querySelector(".key").addEventListener("click", playSound);
+
 document.addEventListener("keydown", playSound);
