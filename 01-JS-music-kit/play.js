b880531786a2b1763d8dee3f277bda41b5ba84d8
Dmitry Vdovichencko <Kenar89@gmail.com>
2019-02-17 09:36:01 +0500

    Restructure JS file. Click event added.

--- 01-JS-music-kit/play.js
+++ 01-JS-music-kit/play.js
@@ -1,46 +1,31 @@
-//create a play function
 
-const findAudioEl=(event)=>{
-	let audioSelector;
+//Find elements function. If we click on element we use key argument, if we click on keyboard - we use event object.
+const findEl=(event,key)=>{
+	let el={};
+	if (key){
+		el.keyEl=document.querySelector(`.key[data-key="${key}"]`);
+		el.audioEl=document.querySelector(`audio[data-key="${key}"]`);
+	}
+	else {
+		el.keyEl=document.querySelector(`.key[data-key="${event.keyCode}"]`);
+		el.audioEl=document.querySelector(`audio[data-key="${event.keyCode}"]`,);
+	}
+	return el;
 
-
-	audioSelector = `audio[data-key="${event.keyCode}"]`;
-
-	
-
-	try{
-    	
-    	playAudio(audioSelector);
-    }
-    catch(e){
-    	console.log(e);
-    	help('.help',true);
-
-    }
 };
-const findKeyEl=(event)=>{
-	let keySelector= `.key[data-key="${event.keyCode}"]`;
-	try{
-  		animateKey(keySelector);
-    }
-    catch(e){
-    	console.log(e);
-    	help('.help',true);
-  
-    }
-};
-const animateKey=(keySelector)=>{
-     	let keyEl=document.querySelector(keySelector);
-    	keyEl.classList.add("key_playing");
+// animate function
+const animateKey=(keyEl)=>{
+       	keyEl.classList.add("key_playing");
     	keyEl.addEventListener('transitionend', function() {
     	keyEl.classList.remove("key_playing");
   		});
 };
-const playAudio=(audioElementSel)=>{
-	let audioEl=document.querySelector(audioElementSel);
+//play audio function with restart playing, when we click on buttons a lot
+const playAudio=(audioEl)=>{
 	audioEl.currentTime=0;
 	audioEl.play();
 };
+// help function - will start, if we click on other keys on keyboard. So if display === false help message will disappear
 const help=(helpSel,display)=>{
 	let helpEl = document.querySelector(helpSel);
 	if(display){
@@ -50,17 +35,26 @@ const help=(helpSel,display)=>{
 		helpEl.classList.contains('help_display') ? helpEl.classList.remove('help_display') : {};	
 	}
 };
+//finally it's controlSound function to manage help, find, play and animate functions
+const controlSound=(event,key)=>{
+	help('.help',false);
+	try{
+		playAudio(findEl(event,key).audioEl);
+		animateKey(findEl(event,key).keyEl);
+    }
+    catch(e){
+    	help('.help',true);
+    }
 
-const playSound=(event)=>{
-
-    
-    help('.help',false);
-	findAudioEl(event);
-	findKeyEl(event);
 
 
 };
-// document.onkeydown = playSound;
 
 
-document.addEventListener("keydown", playSound);
+document.querySelectorAll('.key').forEach(function(item,i,arr){
+	item.addEventListener("click", function(){
+		controlSound(null,this.dataset.key)
+	}, false);
+});
+
+document.addEventListener("keydown", controlSound);
