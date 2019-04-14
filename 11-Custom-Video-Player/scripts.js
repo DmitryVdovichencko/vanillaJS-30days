d551f012ed957b1df229f9e0ec2a5b6458d60c28
Dmitry Vdovichencko <Kenar89@gmail.com>
2019-04-14 14:48:40 +0500

    video progress bar added

--- 11-Custom-Video-Player/scripts.js
+++ 11-Custom-Video-Player/scripts.js
@@ -2,7 +2,7 @@
 const video = document.querySelector('.player__video');
 const play = document.querySelector('.play');
 const progressBar = document.querySelector('.progress__filled');
-
+const bar = document.querySelector('.progress');
 
 // declare functions for custom controls
 function togglePlay(videoEl) {
@@ -22,6 +22,11 @@ function displayTime(videoEl,statusBar){
 	statusBar.style.width = `${currentTime(videoEl)/videoEl.duration*100}%`;
 	
 }
+function changeCurrentTime(event,videoEl,statusBar,bar){
+	console.log(`${document.body.clientWidth} ${( event.clientX-(document.body.clientWidth-bar.offsetWidth)/2 )*100/bar.offsetWidth} - %status width ${bar.offsetWidth} - bar width`);
+	const selTime= (event.clientX-(document.body.clientWidth-bar.offsetWidth)/2 )/bar.offsetWidth;
+	videoEl.currentTime=`${Math.round(selTime*videoEl.duration)}`;
+}
 function control(videoEl){
 	setTimeout(function updateProgress() {
   		displayTime(video,progressBar);
@@ -32,4 +37,5 @@ function control(videoEl){
 
 // add event listeners for control
 play.addEventListener('click', function(){togglePlay(video);});
-video.addEventListener("play", function(){control(video);});
+video.addEventListener('play', function(){control(video);});
+bar.addEventListener('click', function(){changeCurrentTime(event,video,progressBar,bar);});
